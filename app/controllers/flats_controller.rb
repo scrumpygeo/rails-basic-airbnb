04f50ca8497b83a1a9e816c8fc22cf72e8d84944
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-14 17:09:56 +0100

    add new form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,4 @@
 class FlatsController < ApplicationController
-    before_action :find_flat, only: [ :create ]
 
     def index
         @flats = Flat.all
@@ -10,22 +9,18 @@ class FlatsController < ApplicationController
     end
 
     def create
-        @flat = Flat.find(params[:id])
-        @flat = Flat.net(flat_params)
+        @flat = Flat.new(flat_params)
         if @flat.save
-            redirect-to flat_path(@flat)
+            redirect_to flats_path
         else
-            render "new"
+            render :new
         end
-
     end
 
 
     private
 
-    def find_flat
-        @flat = Flat.find(params[:id])
-    end
+
 
     def flat_params
         params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)

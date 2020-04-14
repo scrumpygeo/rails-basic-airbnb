5169243ab9f885788d1590361fe90dc9b42f9d8b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-14 09:57:20 +0100

    create 1 model - flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+Flat.create!(
+    name: 'Airy & Spacious Garden Flat Edinburgh',
+    address: '10 Clifton Gardens Edinburgh EH6 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 55,
+    number_of_guests: 3
+  )  
+
+  Flat.create!(
+    name: 'Modest Clean Tenement Top-Floor Flat Glasgow',
+    address: '122 Tobermory Rd, Glasgow G3 8GD',
+    description: 'An old time experience for this small but comfortable flat. One double beddroom, livingroom, kitchen and bathroom with shower',
+    price_per_night: 35,
+    number_of_guests: 4
+  )  
+
+  Flat.create!(
+    name: 'Nurse Accomodation Edinburgh',
+    address: '13 Hospital Court Edinburgh EH2 4DT',
+    description: 'A functional habitation in nurses residence near Edinburgh Royal',
+    price_per_night: 25,
+    number_of_guests: 1
+  )  
+
+  Flat.create!(
+    name: 'French Style Gite in Scottish Countryside Glasgow',
+    address: '2 Stirling Rd, Glasgow G33 6DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 55,
+    number_of_guests: 4
+  )  
+
+
+
+puts "Created #{Flat.count} flats"

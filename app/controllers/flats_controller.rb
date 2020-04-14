91b5b266147f0b514f835504865ca9d37a32b89b
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-14 15:49:48 +0100

    add new and create actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,33 @@
 class FlatsController < ApplicationController
+    before_action :find_flat, only: [ :create ]
 
     def index
         @flats = Flat.all
+    end
+
+    def new
+        @flat = Flat.new
+    end
+
+    def create
+        @flat = Flat.find(params[:id])
+        @flat = Flat.net(flat_params)
+        if @flat.save
+            redirect-to flat_path(@flat)
+        else
+            render "new"
+        end
+
+    end
+
+
+    private
+
+    def find_flat
+        @flat = Flat.find(params[:id])
+    end
+
+    def flat_params
+        params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
     end
 end

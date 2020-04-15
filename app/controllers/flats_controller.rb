f0aa65ef46cf43a358f24c50f60748db8fa28dd9
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-15 09:25:52 +0100

    style show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,5 @@
 class FlatsController < ApplicationController
+    before_action :find_flat, only: [ :show ]
 
     def index
         @flats = Flat.all
@@ -15,14 +16,21 @@ class FlatsController < ApplicationController
         else
             render :new
         end
+    end 
+
+    def show
+
     end
+    
 
 
     private
-
-
-
+  
     def flat_params
         params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    end
+
+    def find_flat
+        @flat = Flat.find(params[:id])
     end
 end

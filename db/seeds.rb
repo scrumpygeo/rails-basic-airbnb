51211cbcb6ba9c447b38b3b12e89bb0ec76ffb87
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-15 12:41:21 +0100

    add image url to model

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ Flat.create!(
     address: '10 Clifton Gardens Edinburgh EH6 1DT',
     description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
     price_per_night: 55,
-    number_of_guests: 3
+    number_of_guests: 3,
+    picture_url: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?w=1200'
   )  
 
   Flat.create!(
@@ -16,7 +17,8 @@ Flat.create!(
     address: '122 Tobermory Rd, Glasgow G3 8GD',
     description: 'An old time experience for this small but comfortable flat. One double beddroom, livingroom, kitchen and bathroom with shower',
     price_per_night: 35,
-    number_of_guests: 4
+    number_of_guests: 4,
+    picture_url: 'https://images.unsplash.com/photo-1527030280862-64139fba04ca?w=1200'
   )  
 
   Flat.create!(
@@ -24,33 +26,8 @@ Flat.create!(
     address: '13 Hospital Court Edinburgh EH2 4DT',
     description: 'A functional habitation in nurses residence near Edinburgh Royal',
     price_per_night: 25,
-    number_of_guests: 1
-  )  
-
-  Flat.create!(
-    name: 'French Style Gite in Countryside',
-    address: '2 Stirling Rd, Glasgow G33 6DT',
-    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-    price_per_night: 55,
-    number_of_guests: 4
-  )  
-
-
-
-  Flat.create!(
-    name: 'Nurse Accomodation',
-    address: '13 Hospital Court Edinburgh EH2 4DT',
-    description: 'A functional habitation in nurses residence near Edinburgh Royal',
-    price_per_night: 25,
-    number_of_guests: 1
-  )  
-
-  Flat.create!(
-    name: 'French Style Gite in Scottish Countryside Glasgow',
-    address: '2 Stirling Rd, Glasgow G33 6DT',
-    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-    price_per_night: 55,
-    number_of_guests: 4
+    number_of_guests: 1,
+    picture_url: 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?w=1200'
   )  
 
 puts "Created #{Flat.count} flats"
